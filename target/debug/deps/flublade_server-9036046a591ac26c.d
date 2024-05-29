32367b7664eec15ffb04e061d99ae2cff287cba1
Leandro Schmidt <leandro.schmidt@ibrascan.com>
2024-05-29 10:12:06 -0300

    Starting new engine

---
+++ target/debug/deps/flublade_server-9036046a591ac26c.d
@@ -0,0 +1,5 @@
+C:\Users\leandro.schmidt\Documents\Projetos\flublade_server\target\debug\deps\libflublade_server-9036046a591ac26c.rmeta: src\main.rs
+
+C:\Users\leandro.schmidt\Documents\Projetos\flublade_server\target\debug\deps\flublade_server-9036046a591ac26c.d: src\main.rs
+
+src\main.rs:
---
+++ target/debug/deps/flublade_server-e790933d2e65b6bf.d
@@ -0,0 +1,5 @@
+C:\Users\leandro.schmidt\Documents\Projetos\flublade_server\target\debug\deps\libflublade_server-e790933d2e65b6bf.rmeta: src\main.rs
+
+C:\Users\leandro.schmidt\Documents\Projetos\flublade_server\target\debug\deps\flublade_server-e790933d2e65b6bf.d: src\main.rs
+
+src\main.rs:
